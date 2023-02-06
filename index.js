8050708e28c89aa6523caeaf43adbb6490813312
DroidDevRepo <124630272+DroidDevRepo@users.noreply.github.com>
2023-02-06 14:56:56 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+	<meta charset="UTF=8">
+	<meta http-equiv="X-UA-Compatible" ontent="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<title>DEEP LINKS</title>
+</head>
+<body>
+	<p> Wazzam Universe</p>
+</body>
+</html>
